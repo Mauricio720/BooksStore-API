9beae25d9d8a80428d0a62c0f1a3293c3344efe6
Mauricio Ferreira <mauricio-ferreira2015@outlok.com>
2023-05-01 19:38:17 -0300

    feat: config painel to users with type 0

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,9 +5,18 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+
 
 class LoginController extends Controller
 {
+    protected function credentials(Request $request)
+    {
+        $credentials = $request->only($this->username(), 'password');
+        $credentials['type'] = 0;
+
+        return $credentials;
+    }
     /*
     |--------------------------------------------------------------------------
     | Login Controller
