f48cb49b545966876ec18112379c37466ecab2eb
Mauricio Ferreira <mauricio-ferreira2015@outlok.com>
2023-05-06 19:15:00 -0300

    feat: painel routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Painel\BookController;
 use App\Http\Controllers\Painel\UserController;
 use App\Http\Controllers\Painel\HomeController;
+use App\Http\Controllers\Painel\OrdersController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -22,8 +23,16 @@ Auth::routes();
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/orders', [OrdersController::class, 'index'])->name('orders');
+Route::get('/orders/changeStatus/{id}/{status}', [OrdersController::class, 'changeStatus'])->name('changeStatus');
+
+
 Route::any('/addBook', [BookController::class, 'add'])->name('addBook');
 Route::any('/editBook/{id}', [BookController::class, 'edit'])->name('editBook');
 Route::get('/deleteBook/{id}', [BookController::class, 'delete'])->name('deleteBook');
+
 Route::get('/my_profile', [UserController::class, 'myProfile'])->name('myProfile');
+Route::get('/users', [UserController::class, 'usersSite'])->name('usersSite');
+Route::get('/blockUnlock/{id}', [UserController::class, 'blockUnlock'])->name('blockUnlock');
 Route::post('/update_user', [UserController::class, 'updateUser'])->name('updateUser');
