24b64b7b15eb1ef67840762cb62e8c1f478a24c8
Mauricio Ferreira <mauricio-ferreira2015@outlok.com>
2023-06-02 12:30:29 -0300

    fix: change type prices migratione

--- database/migrations/2023_06_02_145119_change_column_price_book.php
+++ database/migrations/2023_06_02_145119_change_column_price_book.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Book;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
@@ -14,6 +15,17 @@ class ChangeColumnPriceBook extends Migration
      */
     public function up()
     {
+        $books=Book::all();
+        foreach ($books as  $book) {
+            $price=$book->price;
+            $price = preg_replace("/[^0-9,]/", "", $price);
+            $price = str_replace(",", ".", $price);
+            $price = (float) $price;
+            
+            $book->price=$price;
+            $book->save();
+        }
+
         DB::statement('ALTER TABLE `books` MODIFY price float');
     }
 
